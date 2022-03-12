3460bda8bf5ec2879f050341d32e624ab4f20ed4
hugosenari <hugosenari@gmail.com>
2022-03-12 02:48:04 -0300

    chore: re index duckduckgo

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Duckduckgo search'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."duckduckgo-master".url = "path:./master";
+  inputs."duckduckgo-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."duckduckgo-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
